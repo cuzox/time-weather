de9078eb5b918cc03c65f4e73943696bfd814648
cuzox <calevime@gmail.com>
2020-05-14 15:59:09 -0400

    Relocate tests to own folder

--- tests.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from 'chai';
-import 'mocha';
-
-it('Should pass', () => {
-  expect('test').to.equal('test');
-});
---
+++ tests/all.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import 'mocha'
+
+
+describe('App', ()=>{
+  it('#run() should return correct time', () => {
+    expect('test').to.equal('test')
+  })
+})
+
+describe('Utils', ()=>{
+  it('#getTimeFromUTCOffset() should return correct time', () => {
+    expect('test').to.equal('test')
+  })
+
+  it('#parseLocations() should return properly parsed locations', () => {
+    expect('test').to.equal('test')
+  })
+})
